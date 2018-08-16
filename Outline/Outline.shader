c0ebbeaa6c546d6ae62125260ee0bef2c592bf18
artium59 <artium59@gmail.com>
2018-08-16 13:24:18 +0900

    Add Outline Shader

---
+++ Outline/Outline.shader
@@ -0,0 +1,76 @@
+﻿Shader "Mobile/Outline" {
+    Properties {
+        // _Color ("Main Color", Color) = (.5,.5,.5,1)
+        // _MainTex ("Texture", 2D) = "white" {}
+        _OutlineColor ("Outline Color", Color) = (1,0,0,1)
+        _OutlineWidth ("Outline Width", Range(0,5.0)) = 0.003
+    }
+    
+    SubShader {
+        Tags { "Queue" = "Transparent" "RenderType" = "Transparent" }
+        
+        Pass {
+            Cull Off
+            ZWrite Off
+            ZTest Always
+            ColorMask 0
+            
+            Stencil {
+                Ref 1
+                Pass Replace
+            }
+        }
+        
+        Pass {
+            Tags { "Queue" = "Transparent+100" "RenderType" = "Transparent" }
+            
+            Cull Off
+            ZWrite Off
+            ZTest Always
+            ColorMask RGB
+            
+            Blend SrcAlpha OneMinusSrcAlpha
+            
+            Stencil {
+                Ref 1
+                Comp NotEqual
+            }
+            
+            CGPROGRAM
+            #include "UnityCG.cginc"
+            
+            #pragma vertex vert
+            #pragma fragment frag
+            
+            struct appdata {
+                float4 vertex : POSITION;
+                float3 normal : NORMAL;
+            };
+    
+            struct v2f {
+                float4 pos : POSITION;
+                float4 color : COLOR;
+                float3 normal : NORMAL;
+            };
+    
+            float _OutlineWidth;
+            float4 _OutlineColor;
+            
+            v2f vert(appdata v) {
+                v2f o;
+                o.pos = UnityObjectToClipPos(v.vertex);
+ 
+	            float3 norm   = mul ((float3x3)UNITY_MATRIX_IT_MV, v.normal);
+	            float2 offset = TransformViewToProjection(norm.xy);
+ 
+	            o.pos.xy += offset * o.pos.z * _OutlineWidth;
+	            o.color = _OutlineColor;
+	            
+	            return o;
+            }
+            
+            half4 frag(v2f i) : COLOR { return i.color; }
+            ENDCG
+        }
+    }
+}
---
+++ Outline/OutlineMaker.cs
@@ -0,0 +1,74 @@
+﻿// #define VISUAL_DEBUG
+
+using UnityEngine;
+
+public class OutlineMaker : MonoBehaviour
+{
+	#region <Consts>
+
+	// Wall's Layer Number
+	private const int LayerNumber = 1 << 1;
+	// Check only walls
+	private const int RayCastHitCandidateMaxNumber = 2;
+	private const string OriginalShaderName = "Mobile/Bumped Diffuse";
+	// DON'T CHANGE
+	private const string OutlineShaderName = "Mobile/Outline";
+
+	#endregion </Consts>
+
+	#region <Fields>
+
+	// body, weapon, shield
+	[SerializeField] private Renderer[] _rendererGroup;
+	
+	private RaycastHit[] _raycastHitGroup;
+	private Transform _transform;
+
+	#endregion </Fields>
+	
+	private void Start()
+	{
+		_raycastHitGroup = new RaycastHit[RayCastHitCandidateMaxNumber];
+		
+		// @SEE: head is the best place, maybe
+		_transform = GetComponent<Transform>();
+	}
+	
+	private void Update()
+	{
+		// _transform.position + new Vector3(0, 2.4f, 0) = skeleton's head tramsform
+		var direction = CameraManager.GetInstance.MainCameraTransform.position - (_transform.position + new Vector3(0, 2.4f, 0));
+		
+#if VISUAL_DEBUG
+		DrawLine(direction);
+#endif
+		
+		var rayCastHitNumber = Physics.RaycastNonAlloc(_transform.position + new Vector3(0, 2.4f, 0), direction, _raycastHitGroup, Mathf.Infinity, LayerNumber);
+
+		// if (rayCastHitNumber <= 0) return;
+		
+		print(rayCastHitNumber.ToString());
+		
+		for (var index = 0; index < _rendererGroup.Length; index++)
+		{
+			// var outlineObjectMaterial = _outlineObjects[index].GetComponent<Renderer>().material;
+	
+			ShaderNameChange(_rendererGroup[index].material, rayCastHitNumber == 0 ? OriginalShaderName : OutlineShaderName);
+			// outlineObjectMaterial.shader = Shader.Find(rayCastHitNumber == 0 ? OriginalShaderName : OutlineShaderName);
+		}
+	}
+
+	private static void ShaderNameChange(Material mat, string shaderName)
+	{
+		if (mat.name == shaderName) return;
+		
+		mat.shader = Shader.Find(shaderName);
+	}
+	
+#if VISUAL_DEBUG
+	private void DrawLine(Vector3 way) {
+		Debug.DrawRay(_transform.position + new Vector3(0, 2.4f, 0), way, Color.red);
+	}
+#endif
+		
+}
