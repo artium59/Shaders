746dd5691b1487e9160e27b4d6e91c337347a3ec
artium59 <artium59@gmail.com>
2018-08-16 13:23:57 +0900

    Add transparent shader

---
+++ Transparent/Transparent.shader
@@ -0,0 +1,53 @@
+﻿Shader "Mobile/Transparent" {
+	Properties {
+		_Color ("Main Color", Color) = (1,1,1,1)
+		_SpecColor ("Spec Color", Color) = (1,1,1,0)
+		_Emission ("Emissive Color", Color) = (0,0,0,0)
+		_Shininess ("Shininess", Range (0.1, 1)) = 0.7
+	    _MainTex ("Base (RGB) Trans (A)", 2D) = "white" {}
+	}
+	
+	SubShader
+	{ 
+		Tags { "Queue"="Transparent" "RenderType"="Transparent" "IgnoreProjector"="True" }
+		LOD 200
+		
+        // Controls whether pixels from this object are written to the depth buffer (default is On).
+        // If you’re drawing solid objects, leave this on.
+        // If you’re drawing semitransparent effects, switch to ZWrite Off
+        Zwrite Off
+        // the value of this stage is multiplied by (1 - source alpha)
+        Blend SrcAlpha OneMinusSrcAlpha
+
+		Pass
+		{
+		    Material {
+		        Diffuse [_Color]
+                Ambient [_Color]
+                Shininess [_Shininess]
+                Specular [_SpecColor]
+                Emission [_Emission]
+		    }
+		    
+		    Lighting On
+            SeparateSpecular On
+		    
+		    // How to combine Texture with Color
+		    // texture: current texture
+		    // https://docs.unity3d.com/Manual/SL-SetTexture.html
+		    SetTexture [_MainTex] {
+		        Combine texture
+		    }
+		    
+		    // previous: result value of first SetTexture function
+		    // Combine A, Z: A is calculate RGB, Z is calculate A 
+		    SetTexture [_MainTex] {
+		        ConstantColor [_Color]
+		        Combine previous, previous * constant
+		    }
+		    
+			// CGPROGRAM
+			// ENDCG
+		}
+	}
+}
---
+++ Transparent/TransparentManager.cs
@@ -0,0 +1,102 @@
+﻿//#define VISUAL_DEBUG
+
+using UnityEngine;
+
+public class TransparentManager : Singleton<TransparentManager> {
+
+    #region <Consts>
+
+    [SerializeField]private const float TransparentDegree = 0.3f;
+    private const int Layermask = 1 << 1;
+    private const int RayCastHitCandidateMaxNumber = 5;
+    
+    #endregion </Consts>
+
+    #region <Fields>
+  
+    private Renderer[] _previousRendererGroup;
+    private RaycastHit[] _raycastHitGroup;
+    
+    #endregion </Fields>
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+        _raycastHitGroup = new RaycastHit[RayCastHitCandidateMaxNumber];
+    }
+    
+    public void Trigger() {
+                             
+        var direction = CameraManager.GetInstance.MainCameraTransform.position 
+                        - PlayerManager.GetInstance.PlayerChampion.GetUnitPosition;
+        
+        #if VISUAL_DEBUG
+                DrawLine(direction);
+        #endif
+        
+        var currentRendererGroup = new Renderer[RayCastHitCandidateMaxNumber];
+        var rayCastHitNumber = Physics.RaycastNonAlloc(PlayerManager.GetInstance.PlayerChampion.GetUnitPosition,
+            direction, _raycastHitGroup, Mathf.Infinity, Layermask);
+        
+        for (var rayCastHitindex = 0; rayCastHitindex < rayCastHitNumber; rayCastHitindex++)
+        {
+            var raycastHit = _raycastHitGroup[rayCastHitindex];
+            var raycastHitRenderer = raycastHit.transform.GetComponent<Renderer>();
+
+            ColorChange(raycastHitRenderer, TransparentDegree);
+            currentRendererGroup[rayCastHitindex] = raycastHitRenderer;
+        }                
+
+        // if _currentRenderGroup contains _previousRenderGroup node.
+        if (_previousRendererGroup != null)
+        {
+            for (var previousIndex = 0;
+                previousIndex < _previousRendererGroup.Length && _previousRendererGroup[previousIndex] != null;
+                ++previousIndex)
+            {
+                var isContained = false;
+                var previousRender = _previousRendererGroup[previousIndex];
+
+                for (var currentIndex = 0;
+                    currentIndex < currentRendererGroup.Length && currentRendererGroup[currentIndex] != null;
+                    ++currentIndex)
+                {
+                    var currentRender = currentRendererGroup[currentIndex];
+
+                    if (previousRender != currentRender) continue;
+
+                    isContained = true;
+                    break;
+                }
+
+                // if not contains,
+                if (!isContained)
+                    ColorChange(previousRender, 1.0f);
+            }
+        }
+
+        // @SEE<Carey>: Find an alternative of this memory allocaton.
+        _previousRendererGroup = currentRendererGroup;
+    }
+    
+    private void ColorChange(Renderer rendererToChange, float degree)
+    {
+        var rendererMaterialGroup = rendererToChange.materials;
+
+        for (var rendererMaterialIndex = 0;
+            rendererMaterialIndex < rendererMaterialGroup.Length;
+            ++rendererMaterialIndex)
+        {
+            var materialColor = rendererMaterialGroup[rendererMaterialIndex].color;
+            materialColor.a = degree;
+            rendererMaterialGroup[rendererMaterialIndex].SetColor("_Color", materialColor);
+        }
+    }
+    
+#if VISUAL_DEBUG
+    private void DrawLine(Vector3 way) {
+        Debug.DrawRay(_playerTransform.position, way, Color.red);
+    }
+#endif
+    
+}
